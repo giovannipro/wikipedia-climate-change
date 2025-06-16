7a2e51124768c99d4b996ecffc41c70a9ac2e25f
giovannipro <g.profet@gmail.com>
2025-06-16 10:45:22 +0200

    add data about restrictions and review data about editors

--- data/extraction/wikipedia_API.py
+++ data/extraction/wikipedia_API.py
@@ -1,43 +1,37 @@
 import requests
 import csv
 import pandas as pd
+import time
 
 wikipedia_link = 'https://en.wikipedia.org/wiki/'
 wikipedia_api = "https://en.wikipedia.org/w/api.php"
 
-import requests
-import time
-
-def get_article_stats(title, delay=0.1):
-
-    params_contributors = {
-        "action": "query",
-        "titles": title,
-        "prop": "contributors",
-        "pclimit": "max",  # Up to 5000 contributors
-        "format": "json"
+def check_article_restrictions(title):
+    
+    params = {
+        'action': 'query',
+        'format': 'json',
+        'prop': 'info',
+        'inprop': 'protection',
+        'titles': title
     }
     
+    response = requests.get(wikipedia_api, params=params)
+    data = response.json()
+    
+    pages = data['query']['pages']
+    for page_id, page_info in pages.items():
+        if 'protection' in page_info:
+            types = [item['type'] for item in page_info['protection']]
+            return types
+        else:
+            return "No restrictions"
+
+def get_edits_editors(title, delay=0.1):
     try:
-        response = requests.get(wikipedia_api, params=params_contributors)
-        response.raise_for_status()
-        contributors_data = response.json()
         
-        unique_editors = []
-        pages = contributors_data["query"]["pages"]
-        
-        for page_id, page_data in pages.items():
-            if page_id == "-1":
-                print(f"Page '{title}' not found")
-                return None
-                
-            if "contributors" in page_data:
-                unique_editors = [contrib["name"] for contrib in page_data["contributors"]]
-        
-        time.sleep(delay)
-        
-        # Get total edits with pagination
         total_edits = 0
+        unique_editors = set()  # Use set for efficient uniqueness tracking
         rvcontinue = None
         
         while True:
@@ -46,7 +40,7 @@ def get_article_stats(title, delay=0.1):
                 "titles": title,
                 "prop": "revisions",
                 "rvlimit": "max",
-                "rvprop": "timestamp",  # Minimal data for counting
+                "rvprop": "user|timestamp",  # Include user to get editor names
                 "format": "json"
             }
             
@@ -59,8 +53,18 @@ def get_article_stats(title, delay=0.1):
             
             pages = revisions_data["query"]["pages"]
             for page_id, page_data in pages.items():
+                if page_id == "-1":
+                    print(f"Page '{title}' not found")
+                    return None
+                    
                 if "revisions" in page_data:
-                    total_edits += len(page_data["revisions"])
+                    revisions = page_data["revisions"]
+                    total_edits += len(revisions)
+                    
+                    # Extract unique editors from revisions
+                    for revision in revisions:
+                        if "user" in revision:
+                            unique_editors.add(revision["user"])
             
             if "continue" in revisions_data:
                 rvcontinue = revisions_data["continue"]["rvcontinue"]
@@ -72,61 +76,14 @@ def get_article_stats(title, delay=0.1):
             'title': title,
             'total_edits': total_edits,
             'unique_editors': len(unique_editors)
-            # 'editors': unique_editors
+            # 'editors': list(unique_editors)
         }
 
-        # print(data)
         return data
         
     except requests.exceptions.RequestException as e:
         print(f"Request error for '{title}': {e}")
         return None
-
-# def get_wikipedia_edits(title):
-
-#     # params_edits = {
-#     #     "action": "query",
-#     #     "titles": title,
-#     #     "prop": "revisions",
-#     #     "rvlimit": "max",
-#     #     "rvprop": "timestamp",  # Minimal data to reduce response size
-#     #     "format": "json"
-#     # }
-
-#     params_editors = {
-#         "action": "query",
-#         "titles": title,
-#         "prop": "contributors",
-#         "pclimit": "max",
-#         "format": "json"
-#     }
-
-#     # total_edits = get_total_edit_count(wikipedia_api, title)
-
-#     # response_a = requests.get(wikipedia_api, params=params_edits).json()
-#     # pages_a = response_a["query"]["pages"]
-#     # for page_id, page_data in pages_a.items():
-#     #     total_edits = len(page_data["revisions"])
-#     #     print(total_edits)
-        
-        
-#     # response_a = requests.get(wikipedia_api, params=params_edits).json()
-#     # page_a = next(iter(response_a["query"]["pages"].values()))
-#     # total_edits = page_a.get("editcount") # page_a.get("edits", "N/A")
-
-#     response_b = requests.get(wikipedia_api, params=params_editors).json()
-#     pages_b = response_b["query"]["pages"]
-
-#     total_editors = 0
-#     for page_id in pages_b:
-#         if "contributors" in pages_b[page_id]:
-#             contributors = pages_b[page_id]["contributors"]
-
-#             unique_editors = [contrib["name"] for contrib in contributors]
-
-#     total_editors = len(unique_editors)
-
-#     # return total_edits, total_editors
 
 def get_wikipedia_issues(title):
         
@@ -211,7 +168,7 @@ def get_wikipedia_extract(title):
 def read_csv_file(file_path):
 
     input_file = file_path + 'input/article_list.tsv'
-    output_file = file_path + 'output/output.tsv'
+    output_file = file_path + 'output/output_2.tsv'
 
     df = pd.read_csv(input_file, delimiter='\t')
     
@@ -227,11 +184,14 @@ def read_csv_file(file_path):
     # if 'issues' not in df.columns:
     #     df['issues'] = None
 
-    if 'edits' not in df.columns:
-        df['edits'] = None
+    # if 'edits' not in df.columns:
+    #     df['edits'] = None
 
-    if 'editors' not in df.columns:
-        df['editors'] = None
+    # if 'editors' not in df.columns:
+    #     df['editors'] = None
+
+    if 'restrictions' not in df.columns:
+        df['restrictions'] = None
     
     count = 0   
     for index, row in df.iterrows():
@@ -253,14 +213,16 @@ def read_csv_file(file_path):
             # data = get_wikipedia_issues(title)
             # df.loc[index, 'issues'] = data
             # print (count, title, data)
+            
+            # data = get_edits_editors(title)
+            # print(count, title, data.get('total_edits'), data.get('unique_editors'))
 
-            data = get_article_stats(title)
-            print(count, title, data.get('total_edits'), data.get('unique_editors'))
+            data = check_article_restrictions(title)
+            restrictions = str(data).replace('[', '').replace(']', '').replace("'", '')
+            print(count, title, restrictions)
 
-            df.loc[index, 'edits'] = data.get('total_edits')
-            df.loc[index, 'editors'] = data.get('unique_editors')
-
-            result_df = df[['title', 'edits', 'editors']]
+            df.loc[index, 'restrictions'] = restrictions
+            result_df = df[['title', 'restrictions']]
 
     result_df.to_csv(output_file, sep='\t', index=False)
 
